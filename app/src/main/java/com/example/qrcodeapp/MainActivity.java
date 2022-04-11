aef5929997969ab6c0da5093997ceb3beb8fb7a6
thisisnotyours <k40001338@gmail.com>
2022-04-11 17:27:06 +0900

    initial commit2

--- app/src/main/java/com/example/qrcodeapp/MainActivity.java
+++ app/src/main/java/com/example/qrcodeapp/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.qrcodeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import needle.Needle;
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,9 +46,55 @@ public class MainActivity extends AppCompatActivity {
 
                 if (etUserId.getText().length() != 0) {
                     if (etUserPw.getText().length() != 0) {
-                        Intent i = new Intent( MainActivity.this, QRCodeActivity.class );
-                        i.putExtra("userId", etUserId.getText().toString());
-                        startActivity(i);
+
+
+                        new AppLogin().execute(etUserId.getText().toString().trim(), etUserPw.getText().toString().trim());
+
+
+                        /**
+                        Needle.onBackgroundThread().execute(new Runnable() {
+                        @Override
+                            public void run() {
+                            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                                    .connectTimeout(2, TimeUnit.MINUTES)
+                                    .readTimeout(1, TimeUnit.MINUTES)
+                                    .writeTimeout(30, TimeUnit.SECONDS)
+                                    .build();
+                            Retrofit retrofit = new Retrofit.Builder()
+                                    .baseUrl(getResources().getString(R.string.base_url))
+                                    .addConverterFactory(GsonConverterFactory.create())
+                                    .build();
+
+                            SpringController controller = retrofit.create(SpringController.class);
+                            String userId = null;
+                            String usesrPw = null;
+
+                            final Call<List<User_InfoVo>> call = controller.app_login(userId, usesrPw);
+                            call.enqueue(new Callback<List<User_InfoVo>>() {
+                                @Override
+                                public void onResponse(Call<List<User_InfoVo>> call, Response<List<User_InfoVo>> response) {
+                                    List<User_InfoVo> list = response.body();
+                                    if ( list == null || list.toString().equals("[null]") ){
+                                        Toast.makeText(MainActivity.this, "get NULL", Toast.LENGTH_SHORT).show();
+                                        Log.d("getUserInfo> ", "get Null");
+                                    }else {
+                                        Log.d("getUserInfo> ", list.get(0).getEmp_id()+", "+list.get(0).getEmp_pw());
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(Call<List<User_InfoVo>> call, Throwable t) {
+
+                                }
+                            });
+
+                            }
+                        });
+                        **/
+
+//                        Intent i = new Intent( MainActivity.this, QRCodeActivity.class );
+//                        i.putExtra("userId", etUserId.getText().toString());
+//                        startActivity(i);
                     }else{
                         Toast.makeText(MainActivity.this, "비밀번호를 다시 확인해주세요.", Toast.LENGTH_SHORT).show();
                     }
@@ -50,4 +109,46 @@ public class MainActivity extends AppCompatActivity {
 
 
     }//onCreate..
+
+
+    private class AppLogin extends AsyncTask<String, Integer, Long> {
+
+        @Override
+        protected Long doInBackground(String... strings) {
+
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .connectTimeout(2, TimeUnit.MINUTES)
+                    .readTimeout(1, TimeUnit.MINUTES)
+                    .writeTimeout(30, TimeUnit.SECONDS)
+                    .build();
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(getResources().getString(R.string.base_url))
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            SpringController controller = retrofit.create(SpringController.class);
+            String userId = strings[0];
+            String userPw = strings[1];
+            final Call<List<User_InfoVo>> call = controller.app_login(userId, userPw);
+            call.enqueue(new Callback<List<User_InfoVo>>() {
+                @Override
+                public void onResponse(Call<List<User_InfoVo>> call, Response<List<User_InfoVo>> response) {
+                    List<User_InfoVo> list = response.body();
+                    if (list == null || list.toString().equals("[null]")) {
+                        Log.d("getUserInfo", "null");
+                    }else {
+                        Log.d("getUserInfo", list.get(0).getEmp_id()+", "+list.get(0).getEmp_pw());
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<List<User_InfoVo>> call, Throwable t) {
+                    Log.d("getUserInfo", "fail : "+t.toString());
+                }
+            });
+
+            return null;
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/qrcodeapp/SpringController.java
+++ app/src/main/java/com/example/qrcodeapp/SpringController.java
@@ -1,22 +1,34 @@
 package com.example.qrcodeapp;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface SpringController {
 
 
-//
+    @GET("barcode/app_Login")
+    Call<List<User_InfoVo>> app_login(@Query("emp_id") String emp_id , @Query("emp_pw") String emp_pw);
+
 //    // Retrofit Helper
-//    OkHttpClient okHttpClient = new OkHttpClient.Builder()
-//            .connectTimeout(1, TimeUnit.MINUTES)
-//            .readTimeout(30, TimeUnit.SECONDS)
-//            .writeTimeout(15, TimeUnit.SECONDS)
-//            .build();
-//
-//    public static final Retrofit retrofit = new Retrofit.Builder()
-////            .baseUrl("http://192.168.0.196:8080/controller/")  //정호대리님 ip
-////             .baseUrl("http://192.168.0.122:8181/controller/")
+    OkHttpClient okHttpClient = new OkHttpClient.Builder()
+        .connectTimeout(1, TimeUnit.MINUTES)
+        .readTimeout(301, TimeUnit.SECONDS)
+        .writeTimeout(15, TimeUnit.SECONDS)
+        .build();
+
+    public static final Retrofit retrofit = new Retrofit.Builder()
+//            .baseUrl("http://192.168.0.196:8080/controller/")  //정호대리님 ip
+             .baseUrl("http://192.168.0.122:8181/controller/")
 //            .baseUrl("http://ierp.interpass.co.kr/controller/")
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .client(okHttpClient)
-//            .build();
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build();
 
 }
